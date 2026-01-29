44fc72476b47edd83e89a0b3a890399f6082d4ab
Artem Bunichev <tembun@bk.ru>
2026-01-30 00:17:40 +0300

    Remove empty lines with trailing whitespaces

--- batd.c
+++ batd.c
@@ -5,23 +5,25 @@
 	means of popping an X(7) powered window up on the screen.
 */
 
+#include<sys/ioctl.h> /* ioctl(2) */
+#include<sys/wait.h> /* wait(2) */
 
-#include<fcntl.h> /* open(2). */
-#include<unistd.h> /* fork(2) and sleep(3). */
-#include<sys/wait.h> /* wait(2). */
-#include<dev/acpica/acpiio.h> /* acpi_battery(4) types. */
-#include<sys/ioctl.h> /* ioctl(2). */
-#include<stdio.h> /* dprintf(3). */
-#include<string.h> /* strlen(3). */
-#include<X11/Xlib.h> /* base Xlib functions (X(7)). */
-#include<X11/Xutil.h> /* XClassHint(3).  */
-#include"config.h" /* custom configuration. */
+#include<dev/acpica/acpiio.h> /* acpi_battery(4) types */
+
+#include<fcntl.h> /* open(2) */
+#include<stdio.h> /* dprintf(3) */
+#include<string.h> /* strlen(3) */
+#include<unistd.h> /* fork(2) and sleep(3) */
+
+#include<X11/Xlib.h> /* base Xlib functions (X(7)) */
+#include<X11/Xutil.h> /* XClassHint(3)  */
+
+#include"config.h" /* custom configuration */
 
 
 /*
 	A structures that holds the information about physical
 	size of a string printed on the screen with the font.
-	
 	It's used by `strsz' function.
 */
 typedef struct {
@@ -55,6 +57,7 @@ Window btn;
 /* button width and height. */
 int bw, bh;
 
+
 /* Graphics Context for main window. */
 GC gc;
 
@@ -76,18 +79,15 @@ void dofont() {
 /*
 	Calculate a physical size of a string when it's printed
 	on the screen with the current font.
-	
 	The second argument is a pointer to resulting structure.
 */
 void strsz(char* msg, sz* ret) {
 	/* dummy variable for values we don't need. */
 	int dum;
-	
 	XCharStruct xch;
 	/* Make a request for string parameters. */
 	XTextExtents(font, msg, strlen(msg), &dum,
 		&dum, &dum, &xch);
-	
 	/* And put them into the resulting structure. */
 	ret->w = xch.rbearing - xch.lbearing;
 	ret->h = xch.ascent + xch.descent;
@@ -114,18 +114,15 @@ void dowin(char* msg) {
 	/* request calculation of the message physical size. */
 	strsz(msg, &msgsz);
 	ww = 2 * WPADX + msgsz.w;
-	
 	/* calculate the physical size on the screen for "OK". */
 	strsz("OK", &oksz);
 	bw = 2 * BPADX + oksz.w;
 	bh = 2 * BPADY + oksz.h;
-	
 	/*
 		now, when we now the size of a button, we can
 		calculate the size of a whole window.
 	*/
 	wh = 2 * WPADY + msgsz.h + WGAP + bh;
-	
 	/*
 		Main window will be positioned at a screen center.
 	*/
@@ -133,25 +130,20 @@ void dowin(char* msg) {
 	/* calculate the actual position for window. */
 	int wx = WidthOfScreen(scr) / 2 - ww / 2;
 	int wy = HeightOfScreen(scr) / 2 - wh / 2;
-	
 	/* create the main window. */
 	w = XCreateSimpleWindow(dpl, DefaultRootWindow(dpl), wx, wy,
 		ww, wh, 0, 0, 0xFFFFFF);
-	
 	/* calculate a position within the main window for button. */
 	int bx = (ww - bw) / 2;
 	int by = wh - WPADY - bh;
-	
 	/*
 		Create the button window.
-		
 		In X11 there are no buttons, we deal only with windows.
 		So, in X's terms, button is a window too - a window, that
 		is a child of our main window.
 	*/
 	btn = XCreateSimpleWindow(dpl, w, bx, by, bw, bh,
 		0, 0, 0x000000);
-	
 	/*
 		Set main window name hints (instance and class),
 		and window name.
@@ -166,14 +158,12 @@ void dowin(char* msg) {
 	XSetClassHint(dpl, w, clshnt);
 	XFree(clshnt);
 	XStoreName(dpl, w, "batd");
-	
 	/*
 		subscribe to `WM_DELETE_WINDOW' event;
 		the window manager reports it when the window is killed.
 	*/
 	wmdel = XInternAtom(dpl, "WM_DELETE_WINDOW", False);
 	XSetWMProtocols(dpl, w, &wmdel, 1);
-	
 	/*
 		Now tell the X server which events we want to receive.
 		For main window we only need the `Expose' event, it'll
@@ -185,17 +175,14 @@ void dowin(char* msg) {
 	XSelectInput(dpl, w, ExposureMask);
 	XSelectInput(dpl, btn, ButtonPressMask | ButtonReleaseMask
 		| ExposureMask);
-	
 	/* And finally display our windows. */
 	XMapWindow(dpl, w);
 	XMapWindow(dpl, btn);
-	
 	XSync(dpl, False);
 }
 
 /*
 	draw a string on the window.
-	
 	`sx' and `sy' are coordinates of a top-left point
 	we want our message to be actually printed at. however,
 	we can not just pass them into the `XDrawString' as is - we
@@ -218,9 +205,7 @@ void dowin(char* msg) {
 void drwmsg(Window w, int sx, int sy, char* msg,
  sz* msz, unsigned long fg) {
 	XSetForeground(dpl, gc, fg);
-	
 	XClearWindow (dpl, w);
-	
 	XDrawString(dpl, w, gc, sx,
 		sy + msz->asc, msg, strlen(msg));
 }
@@ -232,7 +217,6 @@ void drwmsg(Window w, int sx, int sy, char* msg,
 void drwmsgs(char* msg) {
 	/* print the message on the main window. */
 	drwmsg(w, WPADX, WPADY, msg, &msgsz, 0x000000);
-	
 	/* print the "OK" on the button. */
 	drwmsg(btn, BPADX, BPADY, "OK", &oksz, 0xFFFFFF);
 }
@@ -240,10 +224,8 @@ void drwmsgs(char* msg) {
 /*
 	Create an X-powered window, write a message text into it
 	and pop it up to the screen.
-	
 	The window consists of a message itself and a button that
 	says "OK", the click on which will close the window.
-	
 	This function is supposed to be called via fork(2) from the
 	`main' function. `main', in its turn, waits till his child
 	dies and do not dare to check the battery till that.
@@ -256,16 +238,11 @@ int popwin(char* msg) {
 		dprintf(2, "[batd]: can't open display.");
 		return 1;
 	}
-	
 	dofont();
-	
 	dowin(msg);
-	
 	dogc();
-	
 	/* is our button pressed. */
 	char presd = 0;	
-	
 	/* is our main window loop stopped. */
 	char quit = 0;
 	XEvent xev;
@@ -280,16 +257,13 @@ int popwin(char* msg) {
 			}
 			break;
 		}
-		
 		/* redraw everything. */
 		case Expose: {
 			drwmsgs(msg);
 			break;
 		}
-		
 		case ButtonPress: {
 			XButtonEvent xpev = xev.xbutton;
-			
 			/*
 				we don't even check for the window this
 				event took place in, since we've the only
@@ -299,13 +273,10 @@ int popwin(char* msg) {
 			presd = 1;			
 			break;
 		}
-		
 		case ButtonRelease: {
 			XButtonEvent xrev = xev.xbutton;
-			
 			/*
 				we want to register only LMB clicks (Button1 in Xlib).
-				
 				as far as we know that there is only our
 				button receive this event, then the response
 				will contain `x' and `y' that are relative for
@@ -318,33 +289,27 @@ int popwin(char* msg) {
 			 && xrev.y >= 0 && presd) {
 				quit = 1;
 			}
-			
 			presd = 0;
 			break;
 		}
 		}
 	}
-	
 	/* close the display and destroy all the used resources. */
 	XCloseDisplay(dpl);
-	
 	return 1;
 }
 
 int main() {
 	/* file descriptor for ACPI device. */
 	int acpifd;
-	
 	/* data structures we will use to obtain information from it. */
 	union acpi_battery_ioctl_arg batio;
 	struct acpi_battinfo* batinf;
-	
 	acpifd = open("/dev/acpi", O_RDONLY);
 	if (acpifd == -1) {
 		dprintf(2, "[batd]: can't open /dev/acpi file.\n");
 		return 1;
 	}
-	
 	/*
 		here we will but a messages that says that we're low
 		on battery and then pass it to our pop-up window.
@@ -353,7 +318,6 @@ int main() {
 	/* the main program loop. */
 	while(1) {
 		msg = 0;
-		
 		/*
 			Here is *very* important to initialize the `unit' member first.
 			According to ioctl(2) page, it uses a third argument for
@@ -361,10 +325,8 @@ int main() {
 			depends on a particular request we're making. Speaking of
 			a request, acpi_battery(4) manual says that as input argument
 			it takes a battery unit number. So we need to specify it.
-			
 			`ACPI_BATTERY_ALL_UNITS' will obtain a summary about all
 			the battery units attached.
-			
 			See more about this request in /usr/src/sys/dev/acpica/acpi_battery.c.
 		*/
 		batio.unit = ACPI_BATTERY_ALL_UNITS;
@@ -373,11 +335,9 @@ int main() {
 			return 1;
 		}
 		batinf = &(batio.battinfo);
-		
 		/*
 			Complain about low battery only if we're *not*
 			already charging.
-			
 			Note: according to comment notes for battery status macros
 			in /usr/src/sys/dev/acpica/acpiio.h, status `0' does not
 			appear in the ACPI specification and is synthetic, so it
@@ -388,7 +348,6 @@ int main() {
  		 batinf->state != ACPI_BATT_STAT_CHARGING) {
 			msg = LOWMSG;
 		}
-		
 		/*
 			so, we are not low on battery,
 			so sleep for a while and then check again.
@@ -397,7 +356,6 @@ int main() {
 			sleep(INTERVAL);
 			continue;
 		}
-		
 		/*
 			But in case we are low, we want
 			to create a child process that will display
@@ -405,12 +363,10 @@ int main() {
 		*/
 		pid_t pid;
 		pid = fork();
-		
 		if (pid == -1) {
 			dprintf(2, "[batd]: can't fork(2).\n");
 			return 1;
 		}
-		
 		/* We're child. */
 		if (!pid) {
 			/*
@@ -419,7 +375,6 @@ int main() {
 			*/
 			return popwin(msg);
 		}
-		
 		/* We're parent. */
 		else {
 			/*
@@ -430,11 +385,9 @@ int main() {
 			/* dummy value, we won't use it. */
 			int sts;
 			wait(&sts);
-			
 			sleep(INTERVAL);
 		}
 	}
-	
 	/* Yep, that's it. */
 	return 0;
 }
